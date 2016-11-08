038c0759474de2dfafe6aad87c9f7eab2f7de241
TrischaNL <l.trischa@gmail.com>
2016-11-08 11:20:11 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/model/hotel.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by TrischaNL on 11/6/2016.
+ */
+
+public class hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public hotel(String judul, String deskripsi, Drawable foto)
+    {
+        this.judul  = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
